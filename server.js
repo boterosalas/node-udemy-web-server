e27ed61cd4e127b6b8e8799a476900d8fe6adad8
David Botero Salazar <analiticaweb@pragma.com.co>
2020-03-09 15:53:37 -0500

    first commit

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express()
 const hbs = require('hbs')
 require('./hbs/helpers')
 
+const port = process.env.PORT || 3000;
+
 
 app.use(express.static(__dirname+'/public'));
 hbs.registerPartials( __dirname + '/views/partials/')
@@ -23,6 +25,6 @@ app.get('/about', (req, res) => {
   });
 })
  
-app.listen(3000,()=>{
-    console.log('Escuchando peticiones en el puerto 3000')
+app.listen(port,()=>{
+    console.log('Escuchando peticiones en el puerto '+port)
 })
